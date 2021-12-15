2d8f375858071f327ba8448b3b5f00ef38f10d07
Turbayar <turbayar.ganaa@gmail.com>
2021-12-15 16:03:14 +0800

    not finished

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
 function addItem(event){
     event.preventDefault();
     let text = document.getElementById("todo-input");
+    db.collection("todo_items").add({
+       text: text.value,
+       status:"active"
+    })
+    text.value = "";
 }
+
+function getItems(){
+    db.collection("todo_items").onSnapshot((snapshot) => {
+        console.log(snapshot);
+        let items = [];
+        snapshot.docs.forEach((doc) => {
+            items.push({
+                id: doc.id,
+                ...doc.data()
+            })
+        })
+        generateItems(items)
+    })
+}
+
+function generateItems(items){
+   
+
+    let itemsHtml = "";
+    items.forEach((items) => {
+        itemsHTML += `
+        <div class="todo-item">
+            <div class="check">
+              <div class="check-mark checked">
+                <img src="assets/icon-check.svg" />
+              </div>
+            </div>
+            <div class="todo-text checked"> ${items.text} </div>
+          </div>
+        `
+    })
+    document.querySelector(".todo-items").innerHTML = itemsHtml;
+
+}
+
+
+getItems();
